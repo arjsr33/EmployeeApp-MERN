ccf991adc9e92ba48ed5a6cc88c0ced239ab18f3
arjsr33 <arjunsreedharan32@gmail.com>
2024-08-05 16:53:40 +0530

    Final Commit

--- Backend/app.js
+++ Backend/app.js
@@ -12,7 +12,9 @@ const app = express();
 
 // Middleware
 app.use(bodyParser.json());
-app.use(cors());
+app.use(cors({
+  origin: 'https://employeeapp-mern.vercel.app/'
+}));
 
 // DB Config
 const db = process.env.mongodb_URL;
